120e23b988b5ca422fb973ee277c05015f8ed629
Shubham <srsshubh7@gmail.com>
2021-02-25 12:45:01 +0530

    initial commit

---
+++ src/components/WelcomeClass.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react"
+
+export default class WelcomeComponent extends Component {
+    render() {
+        return(
+            <h1>This is Class Component</h1>
+        )
+    }
+}
